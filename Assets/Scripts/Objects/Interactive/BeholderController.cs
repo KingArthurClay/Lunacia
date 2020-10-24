0136227637f5d9272def5044001ae4f6d892761e
Arthur Clay Odom <thekingofthebritons@gmail.com>
2020-10-24 15:27:31 -0400

    Added Placeholder Beholder - No Pathing around Obstacles, Simply Accelerates Towards Player

---
+++ Assets/Scripts/Objects/Interactive/BeholderController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeholderController : MonoBehaviour
+{
+
+    private GameObject player;
+    private Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float normalDrag;
+    [SerializeField]
+    private float stopDrag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponentInChildren<WakingSight>().activeMode == 1) {
+            rigidbody2D.drag = normalDrag;
+            Vector2 direction = (player.transform.position - this.transform.position).normalized;
+            rigidbody2D.AddForce(new Vector3((direction * this.acceleration).x, (direction * this.acceleration).y));
+        } else {
+            rigidbody2D.drag = stopDrag;
+        }
+    }
+}
